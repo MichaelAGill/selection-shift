d12a263cac814ceaa89863d05372e7a47678ebf3
michael gill <m1k4@hotmail.co.uk>
2025-03-26 22:48:13 +0000

    refactor: streamline selection movement commands and update README

--- extension.js
+++ extension.js
@@ -1,80 +1,48 @@
-
 const vscode = require('vscode');
-
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	const moveRight = vscode.commands.registerCommand('extension.moveSelectionRight', async () => {
-	  const editor = vscode.window.activeTextEditor;
-	  if (!editor) return;
-  
-	  const doc = editor.document;
-	  const selections = editor.selections;
-	  const texts = selections.map(sel => doc.getText(sel));
-	  const lengths = texts.map(text => text.length);
-  
-	  await editor.edit(editBuilder => {
-		selections.forEach(sel => editBuilder.delete(sel));
-	  });
-  
-	  const newPositions = selections.map(sel => {
-		const offset = Math.min(doc.getText().length, doc.offsetAt(sel.end) + 1 - lengths[i]);
-		return doc.positionAt(offset);
-	  });
-  
-	  const newSelections = [];
-  
-	  await editor.edit(editBuilder => {
-		texts.forEach((text, i) => {
-		  editBuilder.insert(newPositions[i], text);
-		  const start = newPositions[i];
-		  const end = start.translate(0, text.length);
-		  newSelections.push(new vscode.Selection(start, end));
-		});
-	  });
-  
-	  editor.selections = newSelections;
-	});
-  
-	context.subscriptions.push(moveRight);
-
-	const moveLeft = vscode.commands.registerCommand('extension.moveSelectionLeft', async () => {
+	const moveSelection = async (direction) => {
 		const editor = vscode.window.activeTextEditor;
 		if (!editor) return;
-	  
+
 		const doc = editor.document;
 		const selections = editor.selections;
-	  
 		const texts = selections.map(sel => doc.getText(sel));
-	  
+
 		await editor.edit(editBuilder => {
-		  selections.forEach(sel => editBuilder.delete(sel));
+			selections.forEach(sel => editBuilder.delete(sel));
 		});
-	  
-		const newPositions = selections.map(sel => {
-		  const offset = Math.max(0, doc.offsetAt(sel.start) - 1);
-		  return doc.positionAt(offset);
+
+		const newPositions = selections.map((sel, i) => {
+			const offset = direction === 'right'
+				? Math.min(doc.getText().length, doc.offsetAt(sel.end) + 1 - texts[i].length)
+				: Math.max(0, doc.offsetAt(sel.start) - 1);
+			return doc.positionAt(offset);
 		});
-	  
+
 		const newSelections = [];
-	  
+
 		await editor.edit(editBuilder => {
-		  texts.forEach((text, i) => {
-			editBuilder.insert(newPositions[i], text);
-			const start = newPositions[i];
-			const end = start.translate(0, text.length);
-			newSelections.push(new vscode.Selection(start, end));
-		  });
+			texts.forEach((text, i) => {
+				editBuilder.insert(newPositions[i], text);
+				const start = newPositions[i];
+				const end = start.translate(0, text.length);
+				newSelections.push(new vscode.Selection(start, end));
+			});
 		});
-	  
+
 		editor.selections = newSelections;
-	  });
-	  
-	  context.subscriptions.push(moveLeft);
-  }
+	};
+
+	const moveRight = vscode.commands.registerCommand('selection-shift.moveSelectionRight', () => moveSelection('right'));
+	const moveLeft = vscode.commands.registerCommand('selection-shift.moveSelectionLeft', () => moveSelection('left'));
+
+	context.subscriptions.push(moveRight, moveLeft);
+}
 
 // This method is called when your extension is deactivated
 function deactivate() {}
@@ -82,4 +50,4 @@ function deactivate() {}
 module.exports = {
 	activate,
 	deactivate
-}
+};
